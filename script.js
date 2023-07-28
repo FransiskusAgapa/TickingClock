92fc8741672835f44db5bff1946b5aba7e22c5aa
FransXave <santhothosa@gmail.com>
2023-07-28 15:14:41 -0700

    clock angle calculation solved

--- script.js
+++ script.js
@@ -14,15 +14,37 @@ setInterval(function () {
   var time = getSecondsSinceStartOfDay();
   console.log(time);
 
-  // seconds
+  // SECOND
   let sec = (time / 60) * 360 + 90;
   theSeconds.style.transform = `rotate(${sec}deg)`;
-  // minutes
-  let min = (time / 60) * 360 + ((time / 60) * 6 + 90);
-  theMinutes.style.transform = `rotate(${min})`;
-  // hour
-  let hour = (time / 12) * 360 + (time / 60) * 30 + 90;
-  theHours.style.transform = `rotate(${hour})`;
+
+  // MINUTE
+  // 360 / 60 => 360 is the circle radius, there is 60 minutes in an hour
+  // A
+  // let m = new Date().getMinutes() * (360 / 60);
+  // let min = Math.abs(180 + m);
+  // console.log(`m: ${m}, min = ${min}`);
+
+  // B
+  let min = new Date().getMinutes();
+  let minAngle = Math.abs(180 + new Date().getMinutes() * (360 / 60));
+  console.log(`Minute: ${min}, MinuteAngle = ${minAngle}`);
+
+  theMinutes.style.transform = `rotate(${minAngle}deg)`;
+
+  // HOUR
+  // % 12 => 24 hr notation in a day
+  // A
+  // let h = (new Date().getHours() % 12) * (360 / 12);
+  // let hour = Math.abs(180 + h);
+  // console.log(`h: ${h}, hour = ${hour}`);
+
+  // B
+  let hour = new Date().getHours();
+  let hourAngle = Math.abs(180 + (new Date().getHours() % 12) * (360 / 12));
+  console.log(`Hour: ${hour}, HourAngle = ${hourAngle}`);
+
+  theHours.style.transform = `rotate(${hourAngle}deg)`;
 }, 1000);
 
 /* animate the 'My clock' text */
